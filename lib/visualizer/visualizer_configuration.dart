97f8d21699010cbba934510ed36316eed7b976f7
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-29 11:01:52 +0300

    Define VisualizerConfiguration

---
+++ lib/visualizer/visualizer_configuration.dart
@@ -0,0 +1,9 @@
+class VisualizerConfiguration {
+  final double amplitudeThreshold;
+  final double amplitudeLimit;
+
+  VisualizerConfiguration({
+    required this.amplitudeThreshold,
+    required this.amplitudeLimit,
+  });
+}
